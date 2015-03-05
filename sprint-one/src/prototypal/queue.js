326b7d5aef5bce6599dce26242693a1f3396fa1b
MKS-Student <mks.student@hackreactor.com>
2015-03-05 15:08:23 -0600

    Implemented prototypal stack and queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,22 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var storage = Object.create(queueMethods);
+  storage.oldest = 0;
+  storage.newest = 0;
+  return storage;
 };
 
 var queueMethods = {};
 
-
+queueMethods.enqueue = function (value) {
+  this[this.newest] = value;
+  this.newest++;
+};
+queueMethods.dequeue = function () {
+  var result = this[this.oldest];
+  delete this[this.oldest];
+  result && this.oldest++;
+  return result;
+};
+queueMethods.size = function () {
+  return this.newest - this.oldest;
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  //prototypal link to stack methods 
+  var storage = Object.create(stackMethods);
+  storage.length = 0;
+  return storage;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function (value){
+  var key = this.length;
+  this[key] = value;
+  this.length++;
+
+}
+
+stackMethods.pop = function(){
+  var key = this.length;
+  var result = this[key-1];
+  delete this[key];
+  result && this.length--;
+  return result;
+}
+
+stackMethods.size = function(){
+  return this.length;
+}
+
+
 
 
