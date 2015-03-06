b6788477bdf58947d5713ade00e9af461820927e
MKS-Student <mks.student@hackreactor.com>
2015-03-06 15:05:51 -0600

    Implements tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,28 +1,51 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  extend(newTree, treeMethods);
   return newTree;
 };
-
-
-
 
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var node = new Tree(value);
+  this.children.push(node);
 };
 
 treeMethods.contains = function(target){
+  if(this.value === target){
+    return true;
+  }
+
+  if(this.children[0]){
+    for(var i = 0; i < this.children.length; i++){
+      if (this.children[i].contains(target)) {
+        return true;
+      }
+      continue;
+    }
+  }
+  return false;
 
 };
 
-
 /*
  * Complexity: What is the time complexity of the above functions?
+ *
+ * addChild : O(1)
+ * contains : O(N)
+ *
  */
+
+
+
+///~~~~~~~~~~~~~~
+var extend = function(object1, object2){
+  for( var key in object2){
+    object1[key] = object2[key];
+  }
+  return object1;
+}
+///~~~~~~~~~~~~~~
