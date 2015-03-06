d463d20e65c2f5f4a0dba88e3fff57588df09051
MKS-Student <mks.student@hackreactor.com>
2015-03-05 20:17:18 -0600

    Comments & notes on LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,13 +4,27 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
-  };
+    var node = new Node(value);
 
-  list.removeHead = function(){
+    // get the node at tail 
+    // create new node 
+    // make old tail point to new node
+
+    // if only 1 node added. point head to node aswell
   };
+  //O(1)
+  list.removeHead = function(){
+    //get head's next node.
+    //make head point to next node
+    //delete old head node
+
+  };
+  //O(1)
 
   list.contains = function(target){
+
   };
+  //O(N)
 
   return list;
 };
