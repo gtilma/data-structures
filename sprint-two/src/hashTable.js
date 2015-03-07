7a61981893744c97e69ae657c1226acac05f786c
MKS-Student <mks.student@hackreactor.com>
2015-03-06 20:54:13 -0600

    Major updates on hashtable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,12 +5,25 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(i, v);
+  if(Array.isArray(this._storage.get(i))){
+    var bucket = this._storage.get(i);
+    bucket.push(k,v);
+    this._storage.set(i, bucket );
+
+  }else{
+    this._storage.set(i, [k,v]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(i);
+  var bucket = this._storage.get(k);
+  for(var j = 0; j < bucket.length; j++){
+    if(bucket[j] === k){
+      return bucket[j+1];
+    }
+  }
+  return false;
 };
 
 HashTable.prototype.remove = function(k){
